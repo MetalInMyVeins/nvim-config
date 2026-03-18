ee4f8a8b855f46a4baa7d5ef2d87a4ba776a5568
Hossain Md. Fahim <hossainmdfahim66@gmail.com>
2026-03-18 23:12:16 +0600

    feat: enable snacks

--- init.lua
+++ init.lua
@@ -382,6 +382,7 @@ require("lazy").setup({
     },
     {
       "sphamba/smear-cursor.nvim",
+      enabled = true,
       opts =
       {
         -- Smear cursor when switching buffers or windows.
@@ -414,29 +415,73 @@ require("lazy").setup({
       "karb94/neoscroll.nvim",
       opts = {},
     },
-    --[[{
+    {
       "folke/snacks.nvim",
       priority = 1000,
       lazy = false,
       ---@type snacks.Config
       opts = {
+        picker = {
+          sources = {
+            gh_issue = {
+              -- your gh_issue picker configuration comes here
+              -- or leave it empty to use the default settings
+            },
+            gh_pr = {
+              -- your gh_pr picker configuration comes here
+              -- or leave it empty to use the default settings
+            }
+          }
+        },
+        animate = {
+          enabled = true,
+          duration = 20,
+          fps = 120,
+        },
+        scroll = {
+          enabled = true,
+        },
+        gitbrowse = {
+          enabled = true,
+        },
+        layout = {
+          enabled = true,
+        },
+        statuscolumn = {
+          enabled = false,
+        }
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
-        bigfile = { enabled = true },
-        dashboard = { enabled = true },
-        explorer = { enabled = true },
-        indent = { enabled = true },
-        input = { enabled = true },
-        picker = { enabled = true },
-        notifier = { enabled = true },
-        quickfile = { enabled = true },
-        scope = { enabled = true },
-        scroll = { enabled = true },
-        statuscolumn = { enabled = true },
-        words = { enabled = true },
+        -- bigfile = { enabled = true },
+        -- dashboard = { enabled = true },
+        -- explorer = { enabled = true },
+        -- indent = { enabled = true },
+        -- input = { enabled = true },
+        -- picker = { enabled = true },
+        -- notifier = { enabled = true },
+        -- quickfile = { enabled = true },
+        -- scope = { enabled = true },
+        -- scroll = { enabled = true },
+        -- statuscolumn = { enabled = true },
+        -- words = { enabled = true },
+        -- gh = { enabled = true },
       },
-    },]]--
+      keys = {
+      { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
+      { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+      { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+      { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
+        -- git
+      { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+      { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+      { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+      { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+      { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+      { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+      { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+      },
+    },
     --[[{
       'goolord/alpha-nvim',
       config = function ()
